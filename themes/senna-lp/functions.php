29affb2508234b249be58f52babb038fa50dacec
gurustump <gurustump@gurustump.com>
2014-08-10 04:23:48 -0700

    little changes to get functions and style.css to work correctly in production environment

--- themes/senna-lp/functions.php
+++ themes/senna-lp/functions.php
@@ -1,3 +1,3 @@
-<?phpfunction load_senna_lp_styles() {
-	wp_enqueue_style( 'senna-lp-styles', get_stylesheet_directory_uri() . '/style.css');	wp_enqueue_script( 'senna-lp-actions', get_stylesheet_directory_uri() . '/actions.js');}add_action('wp_enqueue_scripts', 'load_senna_lp_styles')
+<?php function load_senna_lp_styles() {
+	wp_enqueue_style( 'senna-lp-styles', get_stylesheet_directory_uri() . '/style.css');	wp_enqueue_script( 'senna-lp-actions', get_stylesheet_directory_uri() . '/actions.js');}add_action('wp_enqueue_scripts', 'load_senna_lp_styles'); 
 ?>
