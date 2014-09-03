d2aa80bccfe30b94c6efcd028cc304d1715fd147
gurustump <gurustump@gurustump.com>
2014-09-02 22:34:35 -0700

    modifying template and styles for about and contact pages

--- themes/lesliepaugh-bones/page.php
+++ themes/lesliepaugh-bones/page.php
@@ -2,23 +2,26 @@
 
 			<div id="content">
 
-				<div id="inner-content" class="wrap cf">
+				<div id="inner-content" class="wrapper">
 
-						<div id="main" class="m-all t-2of3 d-5of7 cf" role="main">
+						<div id="main" role="main">
 
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
+								<?php if (get_the_title() != 'About') { ?>
 								<header class="article-header">
 
 									<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
 
+									<?php /*
 									<p class="byline vcard">
 										<?php printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), get_the_author_link( get_the_author_meta( 'ID' ) )); ?>
 									</p>
+									*/ ?>
 
-								</header> <?php // end article header ?>
+								</header> <?php } // end article header ?>
 
 								<section class="entry-content cf" itemprop="articleBody">
 									<?php
@@ -50,7 +53,7 @@
 
 								</footer>
 
-								<?php comments_template(); ?>
+								<?php /* comments_template(); */ ?>
 
 							</article>
 
@@ -72,7 +75,7 @@
 
 						</div>
 
-						<?php get_sidebar(); ?>
+						<?php /* get_sidebar(); */ ?>
 
 				</div>
 
