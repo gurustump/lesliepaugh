ebe38f407321ea6ac3f858ecc085592046a8f36d
gurustump <gurustump@gurustump.com>
2014-09-27 11:02:12 -0700

    fixing margins for galleries at 1030 size so image bottom won't go below fold

--- plugins/wordfence/tmp/configCache.php
+++ plugins/wordfence/tmp/configCache.php
@@ -1,4 +1,4 @@
 <?php
 /* Wordfence temporary file security header */
 echo "Nothing to see here!\n"; exit(0);
-?>a:9:{s:9:"cacheType";s:0:"";s:15:"firewallEnabled";s:1:"1";s:6:"apiKey";s:192:"1ce7e873a4b4f7f346c101a7a1c7e5f81b021dbda68965651b6a65239b84a3fcb3c6440142c1e556331d7ac7a28cfd23ccf16b192035ae68c762e32aa3c3ed16236c4636cac9f6c878f0b4419ca7b7d5bb4d591efdf9310aa7e47c586d5e9ce0";s:9:"howGetIPs";s:0:"";s:9:"IPGetFail";s:0:"";s:18:"liveTrafficEnabled";s:1:"1";s:14:"disableCookies";s:1:"0";s:25:"liveTraf_ignorePublishers";s:1:"1";s:11:"other_WFNet";s:1:"1";}
+?>a:7:{s:9:"cacheType";s:0:"";s:15:"firewallEnabled";s:1:"1";s:6:"apiKey";s:192:"1ce7e873a4b4f7f346c101a7a1c7e5f81b021dbda68965651b6a65239b84a3fcb3c6440142c1e556331d7ac7a28cfd23ccf16b192035ae68c762e32aa3c3ed16236c4636cac9f6c878f0b4419ca7b7d5bb4d591efdf9310aa7e47c586d5e9ce0";s:9:"howGetIPs";s:0:"";s:9:"IPGetFail";s:0:"";s:18:"liveTrafficEnabled";s:1:"1";s:14:"disableCookies";s:1:"0";}
